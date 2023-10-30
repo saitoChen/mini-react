2a615ef98ad71f1dd4cdc0b2e4c949b68712b4fc
chenjianfeng <chenjianfeng93@163.com>
2023-10-31 00:35:54 +0800

    feat: init createElement function

---
+++ packages/react/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: chenjianfeng chenjianfeng93@163.com
+ * @Date: 2023-10-30 15:34:42
+ * @Description: react entry
+ */
+import { jsx } from './src/jsx'
+
+export default {
+	version: '0,0,0',
+	createElement: jsx
+}
